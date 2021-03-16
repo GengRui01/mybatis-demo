a3fc5b9f7f5a68a98f5be4ba7fc9a7763fa23ae7
gengrui01 <1207198609@qq.com>
2021-03-16 15:12:23 +0800

    配置文件配置MyBatis

--- src/main/java/StartWithXml.java
+++ src/main/java/StartWithXml.java
@@ -1,11 +1,7 @@
-import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
